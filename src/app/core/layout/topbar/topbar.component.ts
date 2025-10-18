e91512a6d3dbae8bc77fb082f20b917e601b1dff
ajit sonu <ajit.shukla@aristaconsultingus.com>
2025-10-18 23:08:10 +0530

    Upgraded from version 18 to 19

--- src/app/core/layout/topbar/topbar.component.ts
+++ src/app/core/layout/topbar/topbar.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-topbar',
-  standalone: true,
-  imports: [],
-  templateUrl: './topbar.component.html',
-  styleUrl: './topbar.component.scss'
+    selector: 'app-topbar',
+    imports: [],
+    templateUrl: './topbar.component.html',
+    styleUrl: './topbar.component.scss'
 })
 export class TopbarComponent {
 
